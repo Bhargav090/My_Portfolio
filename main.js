56efc93ee6531134be63d2a275d909f23ce33ae8
Bhargav Pathivada <107063758+Bhargav090@users.noreply.github.com>
2024-01-26 11:43:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ var moonimg = document.getElementsByClassName('moonimg')[0];
 
 window.addEventListener('scroll', function() {
   var value = window.scrollY;
+  
+  if (window.innerWidth >= 290 && window.innerWidth <= 400) {
+    value = Math.min(90, Math.max(-20, value));
+  }
+  if (window.innerWidth >= 200 && window.innerWidth <= 289) {
+    value = Math.min(130, Math.max(-20, value));
+  }
+  
   if (value > -20 && value < 141) {
     moonimg.style.marginTop = value * 2.5 + 'px';
   }
+  
   console.log(value);
 });
